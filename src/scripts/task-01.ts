6067ab259acbe4029f3af83814294d07e6226fa3
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-10 17:51:51 +0200

    add task-01

--- src/scripts/task-01.ts
+++ src/scripts/task-01.ts
@@ -1,1 +1,26 @@
+console.log('task-01');
+
+interface IUser {
+  [key: string]: number | string | boolean;
+}
+
+const user: IUser = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+user.mood = 'happy';
+
+user.hobby = 'skydiving';
+
+user.premium = false;
+
+const userKeys: string[] = Object.keys(user);
+
+for (const value of userKeys) {
+  console.log(`${value}: ${user[value]}`);
+}
+
 export {};
