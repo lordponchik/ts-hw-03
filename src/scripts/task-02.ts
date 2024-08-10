ec43018c7893813b032615952e1ac624fc74fe48
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-10 17:56:44 +0200

    add task-02

--- src/scripts/task-02.ts
+++ src/scripts/task-02.ts
@@ -1,1 +1,15 @@
+console.log('task-02');
+
+type CountProps = (obj: {}) => number;
+
+const countProps: CountProps = function (obj) {
+  return Object.keys(obj).length;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+
 export {};
