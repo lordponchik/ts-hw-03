6465dacff771930793c3945ef3283e4c36277f01
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-10 18:22:13 +0200

    add task-04

--- src/scripts/task-04.ts
+++ src/scripts/task-04.ts
@@ -1,1 +1,31 @@
+console.log('task-04');
+console.log('');
+
+type CountTotalSalary = (employees: {}) => number;
+
+const countTotalSalary: CountTotalSalary = function (employees) {
+  return Object.values(employees).reduce((sum: number, el) => {
+    console.log(typeof el);
+    return (sum += el as number);
+  }, 0);
+};
+
 export {};
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
