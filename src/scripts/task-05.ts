e1a7e0f651a16aa3dfd833867cf2cdb46e235f8d
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-11 12:16:42 +0200

    add task-05

--- src/scripts/task-05.ts
+++ src/scripts/task-05.ts
@@ -1,1 +1,36 @@
+type Products = {
+  name: string;
+  price: number;
+  quantity: number;
+}[];
+type GetAllPropValues = (arr: object[], prop: string) => string[] | number[];
+
+const products: Products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues: GetAllPropValues = function (arr, prop) {
+  const array: string[] | number[] = [];
+
+  for (const iterator of arr) {
+    if (prop in iterator) {
+      array.push(iterator[prop as keyof typeof iterator]);
+    } else return [];
+  }
+
+  return array;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
+
 export {};
